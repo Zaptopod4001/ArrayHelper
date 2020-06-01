d1fba063466e238585256371bd7c5be46af9568c
Zaptopod4001 <32018324+Zaptopod4001@users.noreply.github.com>
2020-06-01 14:02:26 +0300

    Update ArrayHelper.cs

--- ArrayHelper.cs
+++ ArrayHelper.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 
 namespace Eses.ArrayHelpers
 {
+    // Copyright Sami S.
 
+    // use of any kind without a written permission 
+    // from the author is not allowed.
+
+    // DO NOT:
+    // Fork, clone, copy or use in any shape or form.
+    
+    
     public class ArrayHelper
     {
 
